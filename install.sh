b675004497ebad42b23d55eb7a2ead955aa587f4
AshkanAbbasi4444 <107311784+AshkanAbbasi4444@users.noreply.github.com>
2025-01-30 15:34:17 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Cloning kaisen-bashrc repository..."
-git clone https://github.com/AshkanAbbasi4444/kaisen-bashrc.git ~/kaisen-bashrc
 
 echo "Backing up existing .bashrc..."
 mv ~/.bashrc ~/.bashrc.backup
